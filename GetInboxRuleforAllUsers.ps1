bbb3f9aff2cf6474f7153230dd1a7f23a7f1d1dc
Lawrence Frias <44064822+lawrencefrias@users.noreply.github.com>
2021-01-04 17:09:32 -0500

    Add files via upload
    A PS script to return all Inbox Rules for All Users in Exchange Online. Formatted for export to a .csv file.

---
+++ GetInboxRuleforAllUsers.ps1
@@ -0,0 +1,5 @@
+﻿Get-ExoMailbox -ResultSize Unlimited |
+Select-Object -ExpandProperty UserPrincipalName |
+Foreach-Object {Get-InboxRule -Mailbox $_ |
+Select-Object -Property MailboxOwnerID,Name,Enabled,From,Description,RedirectTo,ForwardTo} |
+Export-Csv -path c:\scripts\test.csv -NoTypeInformation

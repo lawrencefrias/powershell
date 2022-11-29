2a293fc329e53c0cd53d4a9838bb4eb1b1790ccb
Lawrence Frias <44064822+lawrencefrias@users.noreply.github.com>
2022-11-29 13:40:59 +0100

    Update CreateBulkGuestAccounts.ps1

--- CreateBulkGuestAccounts.ps1
+++ CreateBulkGuestAccounts.ps1
@@ -2,7 +2,7 @@ $invitations = import-csv c:\bulkinvite\invitations.csv
 
 $messageInfo = New-Object Microsoft.Open.MSGraph.Model.InvitedUserMessageInfo
 
-$messageInfo.customizedMessageBody = "Hello. You are invited to the Tilray organization."
+$messageInfo.customizedMessageBody = "Hello. You are invited to the <Name> organization."
 
 foreach ($email in $invitations)
    {New-AzureADMSInvitation `

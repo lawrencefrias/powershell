b7b1d6aaa7db1e56cb1049dda28df82e39f05666
Lawrence Frias <44064822+lawrencefrias@users.noreply.github.com>
2023-08-03 10:49:30 +0200

    Updated scripts to be more generic

--- O365 Scripts/OneDriveUseSilentAccountConfiguration.ps1
+++ O365 Scripts/OneDriveUseSilentAccountConfiguration.ps1
@@ -1,6 +1,6 @@
 $HKLMregistryPath = 'HKLM:\SOFTWARE\Policies\Microsoft\OneDrive'##Path to HKLM keys
 $DiskSizeregistryPath = 'HKLM:\SOFTWARE\Policies\Microsoft\OneDrive\DiskSpaceCheckThresholdMB'##Path to max disk size key
-$TenantGUID = 'e8fc2f00-bdc1-4025-8e28-8b40795e4491'
+$TenantGUID = 'Add-Tenant-ID-Here'
 
 if(!(Test-Path $HKLMregistryPath)){New-Item -Path $HKLMregistryPath -Force}
 if(!(Test-Path $DiskSizeregistryPath)){New-Item -Path $DiskSizeregistryPath -Force}
--- O365 Scripts/ScaryO365Cutover.ps1
+++ O365 Scripts/ScaryO365Cutover.ps1
@@ -1,7 +1,7 @@
 # Must be connected to Office 365 tenant
 
-# Get all tilray.ca mailbox users and their addresses. Export to CSV. 
-$mailboxArray = Get-Mailbox -resultsize unlimited| select-object userprincipalname,alias,@{Name="EmailAddresses";Expression={$_.EmailAddresses | Where-Object {$_ -LIKE "SMTP:*"}}} | where-object -property userprincipalname -eq "danny.klatt@tilray.ca"
+# Get all mailbox users and their addresses. Export to CSV. 
+$mailboxArray = Get-Mailbox -resultsize unlimited| select-object userprincipalname,alias,@{Name="EmailAddresses";Expression={$_.EmailAddresses | Where-Object {$_ -LIKE "SMTP:*"}}} | where-object -property userprincipalname -eq "danny.klatt@domain.ca"
 write-host $mailboxArray
 
 #Iterate through all mailboxes
@@ -10,21 +10,21 @@ foreach($i in $mailboxArray)
 	#Iterate through all email addresses for each mailbox
 	foreach($j in $i.EmailAddresses)
 	{
-		#Write the name of users with @tilray.com emails but not already cut over
-		if($j -like "*@tilray.com" -and $i.UserPrincipalName -notlike "@tilray.com"){
+		#Write the name of users with @domain.com emails but not already cut over
+		if($j -like "*@domain.com" -and $i.UserPrincipalName -notlike "@domain.com"){
 			write-host "if1"
 			$currentEmail = $i.UserPrincipalName
-			$comEmail = $currentEmail.replace("tilray.ca", "tilray.com")
+			$comEmail = $currentEmail.replace("domain.ca", "domain.com")
 			Set-Mailbox $currentEmail -WindowsEmailAddress $comEmail
 			Set-MsolUserPrincipalName -NewUserPrincipalName $comEmail -UserPrincipalName $currentEmail
 			break
 		}
-		#Write the name of users with @tilray.ca emails but without @tilray.com emails
-		if($j -like "*@tilray.ca" -and $j -notlike "@tilray.com"){
+		#Write the name of users with @domain.ca emails but without @domain.com emails
+		if($j -like "*@domain.ca" -and $j -notlike "@domain.com"){
 			# Add a .com email to all accounts with a .ca email based on their existing .ca email
 			write-host "if2"
 			$currentEmail = $i.UserPrincipalName
-			$comEmail = $currentEmail.replace("tilray.ca", "tilray.com")
+			$comEmail = $currentEmail.replace("domain.ca", "domain.com")
 			Set-Mailbox $currentEmail -EmailAddresses @{Add="$comEmail"}
 			Set-Mailbox $currentEmail -WindowsEmailAddress $comEmail
 			Set-MsolUserPrincipalName -NewUserPrincipalName $comEmail -UserPrincipalName $currentEmail
